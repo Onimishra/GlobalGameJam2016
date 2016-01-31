9e22fbe69bf42a7819b46d4b276fc049206127a3
Mathias Vielwerth <onimishra@gmail.com>
2016-01-31 12:17:48 +0100

    Player level renderer fix

--- Assets/PlayerLevelRenderer.cs
+++ Assets/PlayerLevelRenderer.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode]
 public class PlayerLevelRenderer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<Renderer> ().sortingLayerName = "PlayField";
+		foreach(var r in GetComponents<Renderer> ())
+			r.sortingLayerName = "PlayField";
 	}
 	
 	// Update is called once per frame
