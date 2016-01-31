03107521720152435a97c01d5e1e3ec22512f7ee
Mathias Vielwerth <onimishra@gmail.com>
2016-01-31 13:57:10 +0100

    intro shit

---
+++ Assets/Scripts/MenuStepper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuStepper : MonoBehaviour {
+
+	public GameObject firstScreen;
+	public GameObject secondScreen;
+
+	// Use this for initialization
+	void Start () {
+		firstScreen.SetActive (true);
+		secondScreen.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.S) && secondScreen.activeSelf) {
+			SceneManager.LoadScene ("Main");
+		}
+
+		if(Input.GetKeyDown(KeyCode.S) && firstScreen.activeSelf) {
+			firstScreen.SetActive (false);
+			secondScreen.SetActive (true);
+		}
+			
+	}
+}
